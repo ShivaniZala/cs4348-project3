c0f79f7f7e011be0c065cb4efac808401d44bb7e
ShivaniZala <shivani.m.zala@gmail.com>
2024-12-05 16:01:13 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+def print_menu():
+    print("\nIndex File Management System")
+    print("Commands:")
+    print("create - Create a new index file")
+    print("open - Open an existing index file")
+    print("insert - Insert a key/value pair")
+    print("search - Search for a key")
+    print("load - Load key/value pairs from a file")
+    print("print - Print all key/value pairs")
+    print("extract - Save key/value pairs to a file")
+    print("quit - Exit the program")
+
+def main():
+    current_file = None
